b4476a30323a61632718c977df2e53ec82e44f22
Ahmad Radhy <36830633+radhyahmad@users.noreply.github.com>
2022-03-29 23:17:33 +0700

    Update configuration.h

--- MKRGSM_IoT/src/configuration.h
+++ MKRGSM_IoT/src/configuration.h
@@ -6,12 +6,12 @@
 #define GPRS_PASSWORD ""
 
 // Fill in the hostname of your IoT broker
-#define SECRET_BROKER "a1jc2WIhUrQ.iot-as-mqtt.cn-shanghai.aliyuncs.com" 
+#define SECRET_BROKER "" 
  
-#define CLIENT_ID "a1jc2WIhUrQ.mkriot|securemode=2,signmethod=hmacsha256,timestamp=1648375030487|" 
-#define MQTT_USERNAME "mkriot&a1jc2WIhUrQ" 
-#define MQTT_PASSWORD "ff06ba02501af3a4ed82731dd34cfb767620962b8e7cf009dc045c9d60292ba2" 
+#define CLIENT_ID "" 
+#define MQTT_USERNAME "" 
+#define MQTT_PASSWORD "" 
 
 // Fill in the publisher and subscriber topic
-#define PUBLISH_TOPIC "/sys/a1jc2WIhUrQ/mkriot/thing/event/property/post"
-#define SUBSCRIBE_TOPIC "/sys/a1jc2WIhUrQ/mkriot/thing/service/property/set"
+#define PUBLISH_TOPIC ""
+#define SUBSCRIBE_TOPIC ""
